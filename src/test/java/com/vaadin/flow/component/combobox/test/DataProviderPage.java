13e1ef032ae61a935e1f6ea2fb5941488d6f830f
Mehdi Javan <mehdi@vaadin.com>
2018-08-17 10:49:56 +0300

    Using DataProvider::getId in setValue to find the item Fixes #111

---
+++ src/test/java/com/vaadin/flow/component/combobox/test/DataProviderPage.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.combobox.test;
+
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.router.Route;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Route("data-provider")
+public class DataProviderPage extends Div {
+    static final String COMBO_BOX_WITH_GET_ID_ID = "comboBoxWithGetId";
+    static final String COMBO_BOX_WITHOUT_GET_ID_ID = "comboBoxWithoutGetId";
+    static final String SET_VALUE_USING_GET_ID_BUTTON_ID = "setValueUsingGetIdButton";
+    static final String SET_VALUE_USING_REFERENCE_BUTTON_ID = "setValueUsingReferenceButton";
+    static final String SET_VALUE_USING_EQUALS_BUTTON_ID = "setValueUsingEqualsButton";
+
+    public DataProviderPage() {
+        List<TestItem> list = Arrays.asList(new TestItem(1, "a", "First"),
+                new TestItem(2, "b", "Second"), new TestItem(3, "c", "Third"));
+
+        ComboBox<TestItem> comboBoxWithGetId = new ComboBox<>();
+        comboBoxWithGetId.setId(COMBO_BOX_WITH_GET_ID_ID);
+        comboBoxWithGetId.setDataProvider(new ListDataProvider<TestItem>(list) {
+            @Override
+            public Object getId(TestItem item) {
+                return item.getId();
+            }
+        });
+        add(comboBoxWithGetId);
+
+        NativeButton setValueUsingIdButton = new NativeButton(
+                "Set Value Using Id",
+                event -> comboBoxWithGetId.setValue(new TestItem(2)));
+        setValueUsingIdButton.setId(SET_VALUE_USING_GET_ID_BUTTON_ID);
+        add(setValueUsingIdButton);
+
+        ComboBox<TestItem> comboBoxWithoutGetId = new ComboBox<>();
+        comboBoxWithoutGetId.setId(COMBO_BOX_WITHOUT_GET_ID_ID);
+        comboBoxWithoutGetId.setItems(list);
+        add(comboBoxWithoutGetId);
+
+        NativeButton setValueUsingReferenceButton = new NativeButton(
+                "Set Value Using Reference",
+                event -> comboBoxWithoutGetId.setValue(list.get(1)));
+        setValueUsingReferenceButton.setId(SET_VALUE_USING_REFERENCE_BUTTON_ID);
+        add(setValueUsingReferenceButton);
+
+        NativeButton setValueUsingEqualsButton = new NativeButton(
+                "Set Value Using Equals", event -> comboBoxWithoutGetId
+                        .setValue(new TestItem(3, "c", "")));
+        setValueUsingEqualsButton.setId(SET_VALUE_USING_EQUALS_BUTTON_ID);
+        add(setValueUsingEqualsButton);
+    }
+
+    private static class TestItem {
+        private int id;
+        private String name;
+        private String comments;
+
+        public TestItem(int id) {
+            this.id = id;
+        }
+
+        public TestItem(int id, String name, String comments) {
+            this.id = id;
+            this.name = name;
+            this.comments = comments;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name + ", " + comments;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            TestItem testItem = (TestItem) o;
+            return id == testItem.id && Objects.equals(name, testItem.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+}
