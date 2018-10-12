a9a996862a1f57be6ff79bd096e7268ad0b21d5b
Pekka Maanpaa <pekkamaa@vaadin.com>
2018-10-12 13:53:32 +0300

    Rename duplicate js variable

--- src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
+++ src/main/resources/META-INF/resources/frontend/comboBoxConnector.js
@@ -74,11 +74,11 @@ window.Vaadin.Flow.comboBoxConnector = {
         throw 'Got new data to index ' + index + ' which is not aligned with the page size of ' + comboBox.pageSize;
       }
 
-      const firstPage = index / comboBox.pageSize;
+      const firstPageToSet = index / comboBox.pageSize;
       const updatedPageCount = Math.ceil(items.length / comboBox.pageSize);
 
       for (let i = 0; i < updatedPageCount; i++) {
-        let page = firstPage + i;
+        let page = firstPageToSet + i;
         let slice = items.slice(i * comboBox.pageSize, (i + 1) * comboBox.pageSize);
 
         cache[page] = slice;
