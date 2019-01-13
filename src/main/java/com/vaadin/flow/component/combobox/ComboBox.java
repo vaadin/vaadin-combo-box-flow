1d4646d0b14975814edad830f5ab8fe6ef8dbd66
Silver-Wulf <silverwolf8165@gmail.com>
2019-01-12 21:08:11 -0700

    Convert tabs to spaces

--- src/main/java/com/vaadin/flow/component/combobox/ComboBox.java
+++ src/main/java/com/vaadin/flow/component/combobox/ComboBox.java
@@ -461,10 +461,10 @@ public class ComboBox<T> extends GeneratedVaadinComboBox<ComboBox<T>, T>
         }
         
         if(getElement().getProperty("$connector") == null) {
-        	Element e = getElement();
-	        while(e.getParent() != null)
-	        	e = e.getParent();
-	        e.executeJavaScript("window.Vaadin.Flow.comboBoxConnector.initLazy($0);", getElement());
+            Element e = getElement();
+            while(e.getParent() != null)
+                e = e.getParent();
+            e.executeJavaScript("window.Vaadin.Flow.comboBoxConnector.initLazy($0);", getElement());
         }
 
         if (dataCommunicator == null) {
